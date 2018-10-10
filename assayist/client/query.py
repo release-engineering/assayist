26be31638d657dcc8052c7aaf705456866a864cc
mprpic <martin.prpic@gmail.com>
2018-10-10 14:41:15 +0200

    Add API function to search for builds by component and version

--- assayist/client/query.py
+++ assayist/client/query.py
@@ -6,13 +6,45 @@ from assayist.common.models.content import Build
 from assayist.client.error import NotFound
 
 
-def set_connection(neo4j_url):
+def set_connection(neo4j_url):  # pragma: no cover
     """
     Set the Neo4j connection string.
 
     :param str neo4j_url: the Neo4j connection string to configure neomodel with
     """
     neomodel.db.set_connection(neo4j_url)
+
+
+def get_container_by_component(component_name, component_version):
+    """Query for builds by component name and version.
+
+    Finds builds that produced container images which include or embed a component with the
+    specified name and version, and any version preceding that version.
+
+    :param str component_name: name of the component to query for
+    :param str component_version: version of the component to query for
+    :return: list of builds IDs
+    :rtype: list
+    """
+    query = """
+        MATCH (c:Component {{canonical_name: '{name}'}}) <-[:SOURCE_FOR]-
+              (:SourceLocation {{canonical_version: '{version}'}}) <-[:UPSTREAM|EMBEDS*0..]-
+              (target_sl:SourceLocation) <-[:BUILT_FROM]- (build:Build)
+        OPTIONAL MATCH (build) -[:PRODUCED]-> (:Artifact) <-[:EMBEDS*0..]-
+                       (:Artifact) <-[:PRODUCED]- (container_build:Build {{type: 'container'}})
+        OPTIONAL MATCH (target_sl) <-[:BUILT_FROM]- (cf_build:Build {{type: 'container'}})
+        RETURN container_build, cf_build
+    """.format(name=component_name, version=component_version)
+
+    results, _ = neomodel.db.cypher_query(query)
+
+    build_ids = set()
+    for result in results:
+        for node in result:
+            if node:
+                build_ids.add(node['id'])
+
+    return build_ids
 
 
 def get_container_content_sources(build_id):
--- tests/client/test_query.py
+++ tests/client/test_query.py
@@ -2,7 +2,190 @@
 
 from assayist.client import query
 from assayist.common.models.content import Build, Artifact
-from assayist.common.models.source import SourceLocation
+from assayist.common.models.source import SourceLocation, Component
+
+
+def test_get_container_by_component():
+    """Test the get_container_by_component function with a container that includes a component."""
+    # Create the container build entry
+    c_build = Build(id_='123456', type_='container').save()
+
+    c_filename = ('docker-image-sha256:98217b7c89052267e1ed02a41217c2e03577b96125e923e9594'
+                  '1ac010f209ee6.x86_64.tar.gz')
+    c_artifact = Artifact(
+        archive_id='742663', architecture='x86_64', filename=c_filename,
+        type_='container'
+    ).save()
+    c_build.artifacts.connect(c_artifact)
+
+    c_internal_url = ('git://pks.domain.local/containers/rsyslog#'
+                      '3dcd6fc75e674589ac7d2294dbf79bd8ebd459fb')
+    c_internal_source = SourceLocation(url=c_internal_url).save()
+    c_build.source_location.connect(c_internal_source)
+
+    rpm_internal_source_url = (
+        'git://pkgs.devel.redhat.com/rpms/rsyslog#5d99244f963e634c60b458c0c2884ee63d7e8827'
+    )
+    rpm_internal_source = SourceLocation(url=rpm_internal_source_url).save()
+
+    rpm_upstream_url = ('http://www.rsyslog.com/files/download/rsyslog/rsyslog-7.6.7.tar.gz')
+    rpm_upstream_source = SourceLocation(url=rpm_upstream_url, canonical_version='7.6.7').save()
+    rpm_internal_source.upstream.connect(rpm_upstream_source)
+
+    rpm_component = Component(canonical_name='rsyslog', canonical_type='generic',
+                              canonical_namespace='').save()
+    rpm_upstream_source.component.connect(rpm_component)
+
+    rpm_artifact = Artifact(
+        archive_id='760135', architecture='x86_64', filename='rsyslog-8.24.0-34.el7.x86_64.rpm',
+        type_='rpm'
+    ).save()
+
+    rpm_build = Build(id_='789012', type_='rpm').save()
+    rpm_build.artifacts.connect(rpm_artifact)
+    rpm_build.source_location.connect(rpm_internal_source)
+
+    c_artifact.embedded_artifacts.connect(rpm_artifact)
+
+    assert query.get_container_by_component('rsyslog', '7.6.7') == {'123456'}
+
+
+def test_get_container_with_preceding_versions():
+    """Test the get_container_by_component function with a component with preceding versions."""
+    pass
+
+
+def test_get_container_by_embedded_component():
+    """Test the get_container_by_component function with a container that embeds a component."""
+    # Set up a layered container image build, build, whose single
+    # image embeds an RPM and a Go executable. Its parent image also
+    # embeds an RPM.
+    #
+    # First, a parent image, parent:
+    #   (parent:Artifact) <-- (:Build) <-- (:SourceLocation)
+    #
+    # Then the container itself, image:
+    #   (image:Artifact) <-- (:Build) <-- (:SourceLocation)
+    #   (a:Artifact) <-[:EMBEDS]- (parent:Artifact)
+    #
+    # And let's have the parent image embed an RPM, bash:
+    #   (parent:Artifact) <-[:EMBEDS]- (bash_rpm:Artifact)
+    #       <-- (:Build) <-- (internal:SourceLocation)
+    #       <-- (upstream:SourceLocation) <-- (:Component)
+
+    # bash RPM for parent image
+    bash_build = Build(id_='1000', type_='rpm').save()
+    bash_rpm = Artifact(
+        archive_id='1001',
+        architecture='x86_64',
+        filename='bash-4.2.46-30.el7.x86_64.rpm',
+        type_='rpm').save()
+    bash_build.artifacts.connect(bash_rpm)
+    bash_upstream_url = 'ftp://ftp.gnu.org/gnu/bash/bash-4.2.46.tar.gz'
+    bash_upstream_source = SourceLocation(url=bash_upstream_url,
+                                          canonical_version='4.2.46').save()
+    bash_internal_url = 'git://pkgs.domain.local/rpms/bash#5f22bafc903fd0343640a6d7e25c87e32e504b9c'
+    bash_internal_source = SourceLocation(url=bash_internal_url).save()
+    bash_build.source_location.connect(bash_internal_source)
+    bash_internal_source.upstream.connect(bash_upstream_source)
+    bash_component = Component(canonical_type='generic', canonical_namespace='',
+                               canonical_name='bash').save()
+    bash_upstream_source.component.connect(bash_component)
+
+    # Parent image
+    parent_build = Build(id_='2000', type_='container').save()
+    container_filename = 'docker-image-sha256:' + '0' * 64 + '.x86_64.tar.gz'
+    parent = Artifact(
+        archive_id='2001',
+        architecture='x86_64',
+        filename=container_filename,
+        type_='container').save()
+    parent_build.artifacts.connect(parent)
+    container_internal_url = 'git://pkgs.domain.local/containers/parent#...'
+    container_internal_source = SourceLocation(url=container_internal_url).save()
+    parent_build.source_location.connect(container_internal_source)
+    parent.embedded_artifacts.connect(bash_rpm)  # bash RPM is installed
+
+    # etcd RPM for container image we'll query
+    etcd_build = Build(id_='3000', type_='rpm').save()
+    etcd_rpm = Artifact(
+        archive_id='3001',
+        architecture='x86_64',
+        filename='etcd-3.2.22-1.el7.x86_64.rpm',
+        type_='rpm').save()
+    etcd_build.artifacts.connect(etcd_rpm)
+
+    # Add an additional architecture that is not used by the build
+    # we'll query about. This is to test we are not selecting
+    # artifacts we should not be.
+    etcd_unused_rpm = Artifact(
+        archive_id='3002',
+        architecture='ppc64le',
+        filename='etcd-3.2.22-1.el7.ppc64le.rpm',
+        type_='rpm').save()
+    etcd_build.artifacts.connect(etcd_unused_rpm)
+
+    # Sources for etcd
+    etcd_upstream_url = ('https://github.com/coreos/etcd/archive/'
+                         '1674e682fe9fbecd66e9f20b77da852ad7f517a9/etcd-1674e68.tar.gz')
+    etcd_upstream_source = SourceLocation(url=etcd_upstream_url,
+                                          canonical_version='3.2.22').save()
+    etcd_internal_url = 'git://pkgs.domain.local/rpms/etcd#84858fb38a89e1177b0303c675d206f90f6a83e2'
+    etcd_internal_source = SourceLocation(url=etcd_internal_url).save()
+    etcd_build.source_location.connect(etcd_internal_source)
+    etcd_internal_source.upstream.connect(etcd_upstream_source)
+    etcd_component = Component(canonical_type='github', canonical_namespace='coreos',
+                               canonical_name='etcd').save()
+    etcd_upstream_source.component.connect(etcd_component)
+
+    # The container itself
+    container_build = Build(id_='4000', type_='container').save()
+    container_filename = 'docker-image-sha256:' + '1' * 64 + '.x86_64.tar.gz'
+    image = Artifact(
+        archive_id='4001',
+        architecture='x86_64',
+        filename=container_filename,
+        type_='container').save()
+    container_build.artifacts.connect(image)
+    container_internal_url = ('git://pkgs.domain.local/containers/image#...')
+    container_internal_source = SourceLocation(url=container_internal_url).save()
+    container_build.source_location.connect(container_internal_source)
+    image.embedded_artifacts.connect(parent)  # inherits from parent image
+    image.embedded_artifacts.connect(etcd_rpm)  # etcd RPM is installed
+
+    # Go executable
+    # Based on output from backvendor:
+    # *github.com/jimmidyson/configmap-reload@043045da[...] =v0.2.2 ~v0.2.2
+    # golang.org/x/sys@9c60d1c[...] ~v0.0.0-0.20150901164945-9c60d1c508f5
+    # gopkg.in/fsnotify.v1@c282820[...] =v1.4.7 ~v1.4.7
+    configmap_reload = SourceLocation(url='https://github.com/jimmidyson/configmap-reload',
+                                      canonical_version='v0.2.2').save()
+    configmap_reload.component.connect(
+        Component(canonical_type='golang',
+                  canonical_namespace='github.com/jimmidyson',
+                  canonical_name='configmap-reload').save())
+    x_sys = SourceLocation(url='https://go.googlesource.com/sys',
+                           canonical_version='v0.0.0-0.20150901164945-9c60d1c508f5').save()
+    x_sys.component.connect(
+        Component(canonical_type='golang',
+                  canonical_namespace='golang.org/x',
+                  canonical_name='sys').save())
+    fsnotify = SourceLocation(url='https://gopkg.in/fsnotify.v1',
+                              canonical_version='v1.4.7').save()
+    fsnotify.component.connect(
+        Component(canonical_type='golang',
+                  canonical_namespace='gopkg.in',
+                  canonical_name='fsnotify.v1').save())
+
+    container_internal_source.upstream.connect(configmap_reload)
+    container_internal_source.embedded_source_locations.connect(x_sys)
+    container_internal_source.embedded_source_locations.connect(fsnotify)
+
+    assert query.get_container_by_component('fsnotify.v1', 'v1.4.7') == {'4000'}
+    assert query.get_container_by_component('sys',
+                                            'v0.0.0-0.20150901164945-9c60d1c508f5') == {'4000'}
+    assert query.get_container_by_component('configmap-reload', 'v0.2.2') == {'4000'}
+    assert query.get_container_by_component('bash', '4.2.46') == {'4000', '2000'}
 
 
 def test_get_container_sources():
