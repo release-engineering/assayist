129303fdfc4e474a842bc1241a45e2e1a4c72e0e
mprahl <mprahl@users.noreply.github.com>
2018-10-24 08:39:42 -0400

    Add the container RPM analyzer
    This analyzer checks for the installed RPMs in the container layer, and
    adds entries in the graph database and claims the files the RPMs installed.
    
    Addresses FACTORY-3000

---
+++ assayist/processor/container_rpm_analyzer.py
@@ -0,0 +1,130 @@
+# SPDX-License-Identifier: GPL-3.0+
+
+from assayist.processor.base import Analyzer
+from assayist.processor.utils import get_koji_session
+from assayist.processor.logging import log
+from assayist.common.models import content
+
+
+class ContainerRPMAnalyzer(Analyzer):
+    """Analyze the RPMs in a container image layer."""
+
+    def run(self):
+        """Start the container RPM analyzer."""
+        build_info = self.read_metadata_file(self.BUILD_FILE)
+        build_id = build_info['id']
+        if not self.is_container_build(build_info):
+            log.info(f'Skipping build {build_id} because the build is not a container')
+            return
+
+        self.koji_session = get_koji_session()
+        # Create a mapping of arch to archive (container image) so we can easily map to the
+        # parent container archives in a future loop
+        arch_to_archive = {}
+        not_container_msg = 'Skipping archive {0} since it\'s not a container image'
+        for archive in self.read_metadata_file(self.ARCHIVE_FILE):
+            if not self.is_container_archive(archive):
+                log.debug(not_container_msg.format(archive['id']))
+                continue
+            arch = archive['extra']['image']['arch']
+            if arch in arch_to_archive:
+                log.error(
+                    f'Build {build_id} has more than one container image with the arch {arch}')
+                continue
+            arch_to_archive[arch] = archive
+
+        parent_build_id = build_info['extra']['image'].get('parent_build_id')
+        # If there is a parent to this image, then only get the RPMs installed in this layer
+        # and mark them as embedded artifacts on this container image
+        if parent_build_id is not None:
+            # Find the RPMs installed in this layer versus the parent image
+            for archive in self.koji_session.listArchives(parent_build_id):
+                if not self.is_container_archive(archive):
+                    log.debug(not_container_msg.format(archive['id']))
+                    continue
+                arch = archive['extra']['image']['arch']
+                if arch not in arch_to_archive:
+                    log.debug(
+                        f'The parent build {parent_build_id} contains an extra arch of {arch}')
+                    continue
+
+                rpms = self._get_rpms_diff(archive['id'], arch_to_archive[arch]['id'])
+                self._process_embedded_rpms(arch_to_archive[arch], rpms)
+        # If there is no parent, then this is a base image. Just get all the RPMs installed in
+        # the image and mark them as embedded artifacts in this container image.
+        else:
+            for archive in arch_to_archive.values():
+                rpms = self.koji_session.listRPMs(imageID=archive['id'])
+                self._process_embedded_rpms(archive, rpms)
+
+    def _process_embedded_rpms(self, container_archive, rpms):
+        """
+        Add the nodes and relationships in Neo4j and claim the files these RPMs install.
+
+        :param dict container_archive: the Koji archive of the container these RPMs are embedded in
+        :param list rpms: the list of RPMs that are embedded in the container
+        """
+        artifact_obj = content.Artifact.get_or_create({
+            'archive_id': container_archive['id'],
+            'type_': 'container',
+        })[0]
+        # Dictionary to cache Neo4j Build objects
+        build_id_to_obj = {}
+        for rpm in rpms:
+            rpm_artifact_obj = self.create_or_update_rpm_artifact_from_rpm_info(rpm)
+            artifact_obj.embedded_artifacts.connect(rpm_artifact_obj)
+
+            if rpm['build_id'] not in build_id_to_obj:
+                build_id_to_obj[rpm['build_id']] = content.Build.get_or_create({
+                    'id_': rpm['build_id'], 'type_': 'rpm'})[0]
+            build_id_to_obj[rpm['build_id']].artifacts.connect(rpm_artifact_obj)
+
+            # Claim the files these RPMs installed in the container image layer
+            for file_ in self.koji_session.listRPMFiles(rpm['id']):
+                file_path = file_['name']
+                self.claim_container_file(container_archive, file_path)
+
+    def _get_rpms_diff(self, parent_archive_id, child_archive_id):
+        """
+        Determine the RPMs installed in the "child" container image layer.
+
+        :param int parent_archive_id: the archive ID of the parent container image layer
+        :param int child_archive_id: the archive ID of the child container image layer
+        :return: a list of the RPMs (Koji RPM info dictionaries) installed in the child container
+            image layer
+        :rtype: list
+        """
+        parent_rpm_ids = set()
+        for rpm in self.koji_session.listRPMs(imageID=parent_archive_id):
+            parent_rpm_ids.add(rpm['id'])
+
+        child_rpm_ids = set()
+        id_to_rpm = {}
+        for rpm in self.koji_session.listRPMs(imageID=child_archive_id):
+            id_to_rpm[rpm['id']] = rpm
+            child_rpm_ids.add(rpm['id'])
+
+        diff_rpms = []
+        for rpm_id in (child_rpm_ids - parent_rpm_ids):
+            diff_rpms.append(id_to_rpm[rpm_id])
+        return diff_rpms
+
+    @staticmethod
+    def is_container_archive(archive):
+        """
+        Inspect the archive to see if its a container archive.
+
+        :param dict archive: the Koji archive to inspect
+        :return: a boolean determining if it's a container archive
+        :rtype: bool
+        """
+        if archive['btype'] != 'image':
+            return False
+
+        try:
+            archive['extra']['image']['arch']
+            return True
+        # If archive['extra'] is None, then a TypeError is raised. If one of the keys is missing,
+        # then a KeyError is raised.
+        except (TypeError, KeyError):
+            return False
---
+++ tests/processor/test_container_rpm_analyzer.py
@@ -0,0 +1,172 @@
+# SPDX-License-Identifier: GPL-3.0+
+
+import mock
+
+from assayist.processor.container_rpm_analyzer import ContainerRPMAnalyzer
+from assayist.common.models import content
+
+
+def test_get_rpms_diff():
+    """Test that the _get_rpms_diff method returns the correct output."""
+    mock_session = mock.Mock()
+    mock_session.listRPMs.side_effect = [
+        [{'id': 123}, {'id': 124}],
+        [{'id': 123}, {'id': 124}, {'id': 125}, {'id': 126}],
+    ]
+    expected = [{'id': 125}, {'id': 126}]
+    analyzer = ContainerRPMAnalyzer()
+    analyzer.koji_session = mock_session
+    assert analyzer._get_rpms_diff(1, 2) == expected
+
+
+@mock.patch('assayist.processor.container_rpm_analyzer.Analyzer.claim_container_file')
+def test_process_embedded_rpms(mock_claim_cf):
+    """Test that the _process_embedded_rpms method creates the correct entries in Neo4j."""
+    mock_session = mock.Mock()
+    mock_session.listRPMFiles.side_effect = [
+        [{'name': '/etc/app/app.conf'}, {'name': '/usr/bin/app'}],
+        [{'name': '/etc/app2/app.conf'}, {'name': '/usr/bin/app2'}]
+    ]
+    archive = {'id': 1234, 'btype': 'container', 'extra': {'image': {'arch': 'x86_64'}}}
+    rpms = [
+        {
+            'id': 34,
+            'name': 'app',
+            'version': '1.2.3',
+            'release': 1,
+            'arch': 'x86_64',
+            'payloadhash': '123456789abcdef',
+            'build_id': 2,
+        },
+        {
+            'id': 35,
+            'name': 'app2',
+            'version': '2.3.4',
+            'release': 1,
+            'arch': 'x86_64',
+            'payloadhash': '3456789abcdef12',
+            'build_id': 3,
+        }
+    ]
+
+    analyzer = ContainerRPMAnalyzer()
+    analyzer.koji_session = mock_session
+    analyzer._process_embedded_rpms(archive, rpms)
+
+    container = content.Artifact.nodes.get_or_none(archive_id=1234, type_='container')
+    assert container is not None
+    # Make sure the two RPMs that were passed in are embedded in the container
+    assert len(container.embedded_artifacts) == 2
+    embedded_artifact, embedded_artifact2 = container.embedded_artifacts.all()
+    assert embedded_artifact.archive_id == '35'
+    assert embedded_artifact.architecture == 'x86_64'
+    assert embedded_artifact.filename == 'app2-2.3.4-1.x86_64.rpm'
+    assert embedded_artifact.type_ == 'rpm'
+    assert embedded_artifact.checksums[0].checksum == '3456789abcdef12'
+    assert embedded_artifact2.archive_id == '34'
+    assert embedded_artifact2.architecture == 'x86_64'
+    assert embedded_artifact2.filename == 'app-1.2.3-1.x86_64.rpm'
+    assert embedded_artifact2.type_ == 'rpm'
+    assert embedded_artifact2.checksums[0].checksum == '123456789abcdef'
+    # Make sure claim_container_file was called once for each RPM file
+    assert mock_claim_cf.call_count == 4
+
+
+@mock.patch('assayist.processor.base.Analyzer.read_metadata_file')
+@mock.patch('assayist.processor.container_rpm_analyzer.get_koji_session')
+@mock.patch('assayist.processor.container_rpm_analyzer.ContainerRPMAnalyzer._get_rpms_diff')
+@mock.patch('assayist.processor.container_rpm_analyzer.ContainerRPMAnalyzer._process_embedded_rpms')
+def test_run(mock_p_e_rpms, mock_get_diff, mock_get_session, mock_read_md_file):
+    """Test the core logic in the run method when the image isn't a base image."""
+    mock_session = mock.Mock()
+    mock_session.listArchives.return_value = [
+        {
+            'id': 3,
+            'extra': {'image': {'arch': 'x86_64'}},
+            'filename': 'docker-image-sha256-523456789abcde4',
+            'btype': 'image',
+        },
+        {
+            'id': 4,
+            'extra': {'image': {'arch': 's390x'}},
+            'filename': 'docker-image-sha256-63456789abcdef2',
+            'btype': 'image',
+        }
+    ]
+    mock_get_session.return_value = mock_session
+
+    mock_read_md_file.side_effect = [
+        {
+            'id': 1234,
+            'extra': {'container_koji_task_id': 123456, 'image': {'parent_build_id': 22}}
+        },
+        [
+            {
+                'id': 1,
+                'extra': {'image': {'arch': 'x86_64'}},
+                'filename': 'docker-image-sha256-123456789abcdef',
+                'btype': 'image',
+            },
+            {
+                'id': 2,
+                'extra': {'image': {'arch': 's390x'}},
+                'filename': 'docker-image-sha256-33456789abcdef2',
+                'btype': 'image',
+            }
+        ]
+    ]
+
+    analyzer = ContainerRPMAnalyzer()
+    analyzer.run()
+    # Make sure read_metadata_file was called twice, once for the build info and the other for the
+    # archives
+    assert mock_read_md_file.call_count == 2
+    # Make sure _process_embedded_rpms and _get_rpms_diff were called twice, once for each arch
+    assert mock_p_e_rpms.call_count == 2
+    assert mock_get_diff.call_count == 2
+    # Make sure listArchives was called once for the parent
+    mock_session.listArchives.assert_called_once()
+
+
+@mock.patch('assayist.processor.base.Analyzer.read_metadata_file')
+@mock.patch('assayist.processor.container_rpm_analyzer.get_koji_session')
+@mock.patch('assayist.processor.container_rpm_analyzer.ContainerRPMAnalyzer._get_rpms_diff')
+@mock.patch('assayist.processor.container_rpm_analyzer.ContainerRPMAnalyzer._process_embedded_rpms')
+def test_run_parent_image(mock_p_e_rpms, mock_get_diff, mock_get_session, mock_read_md_file):
+    """Test the core logic in the run method when the image is a base image (no parent)."""
+    mock_session = mock.Mock()
+    mock_get_session.return_value = mock_session
+
+    mock_read_md_file.side_effect = [
+        {
+            'id': 1234,
+            'extra': {'container_koji_task_id': 123456, 'image': {}}
+        },
+        [
+            {
+                'id': 3,
+                'extra': {'image': {'arch': 'x86_64'}},
+                'filename': 'docker-image-sha256-523456789abcde4',
+                'btype': 'image',
+            },
+            {
+                'id': 4,
+                'extra': {'image': {'arch': 's390x'}},
+                'filename': 'docker-image-sha256-63456789abcdef2',
+                'btype': 'image',
+            }
+        ]
+    ]
+
+    analyzer = ContainerRPMAnalyzer()
+    analyzer.run()
+    # Make sure read_metadata_file was called twice, once for the build info and the other for the
+    # archives
+    assert mock_read_md_file.call_count == 2
+    # Make sure _process_embedded_rpms was called twice, once for each arch
+    assert mock_p_e_rpms.call_count == 2
+    # Make sure listArchives was not called since that information is cached for the current layer
+    mock_session.listArchives.assert_not_called()
+    # Make sure _get_rpms_diff was not called since that only gets called when the image has a
+    # parent
+    mock_get_diff.assert_not_called()
