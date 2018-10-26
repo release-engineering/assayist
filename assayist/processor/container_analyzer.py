92188c9b1d4512c8f8c5e919f3ca572944b97082
mprpic <martin.prpic@gmail.com>
2018-10-26 17:10:24 +0200

    Add container analyzer

---
+++ assayist/processor/container_analyzer.py
@@ -0,0 +1,101 @@
+# SPDX-License-Identifier: GPL-3.0+
+
+from assayist.common.models import content
+from assayist.processor.base import Analyzer
+from assayist.processor.logging import log
+from assayist.processor.utils import get_koji_session
+
+
+class ContainerAnalyzer(Analyzer):
+    """Analyzes the parent builds of a specific build."""
+
+    def __init__(self):
+        """Declare instance attributes."""
+        self.koji_session = None
+
+    def run(self):
+        """Start the container analyzer."""
+        build_info = self.read_metadata_file(self.BUILD_FILE)
+        build_id = build_info['id']
+
+        if not self.is_container_build(build_info):
+            log.info(f'Skipping build {build_id} because the build is not a container')
+            return
+
+        # If this build has no parent image build, there is nothing to do here.
+        parent_build_id = build_info['extra']['image'].get('parent_build_id')
+        if parent_build_id is None:
+            return
+
+        # This container's build is assumed to exist since it is created by the main analyzer.
+        build = content.Build.nodes.get(id_=build_id)
+
+        # Process parent build and embed all artifacts of the parent build to the artifacts of
+        # this build's artifacts.
+        arch_to_artifact = self._create_or_update_parent(parent_build_id)
+
+        for archive in build.artifacts.filter(type_='container').all():
+            related_archive = arch_to_artifact.get(archive.architecture)
+            if not related_archive:
+                log.error('no artifact to link to, architecture does not exist in parent build')
+                continue
+
+            archive.embedded_artifacts.connect(related_archive)
+
+        # Process parent builds used as buildroots (those specified in `parent_image_builds`
+        # besides the `parent_build_id`. Embed all artifacts of each parent build as buildroot
+        # artifacts of this build's artifacts.
+        parent_image_builds = build_info['extra']['image']['parent_image_builds'].values()
+        parent_image_builds_ids = {build['id'] for build in parent_image_builds
+                                   if build['id'] != parent_build_id}
+
+        for buildroot_parent_build_id in parent_image_builds_ids:
+            arch_to_artifact = self._create_or_update_parent(buildroot_parent_build_id)
+
+            for archive in build.artifacts.filter(type_='container').all():
+                related_archive = arch_to_artifact.get(archive.architecture)
+                if not related_archive:
+                    log.error('no artifact to link to, architecture does not exist in parent build')
+                    continue
+
+                archive.buildroot_artifacts.connect(related_archive)
+
+    def _create_or_update_parent(self, build_id):
+        """Create or update a parent build and its archives (container images).
+
+        :param build_id: build ID of the parent build to process
+        :return: dictionary of container image artifacts indexed by architectures
+        :rtype: dict
+        """
+        parent_build = content.Build.get_or_create({
+            'id_': build_id,
+            'type_': 'buildContainer',
+        })[0]
+
+        if not self.koji_session:
+            self.koji_session = get_koji_session()
+
+        archives = self.koji_session.listArchives(build_id)
+        arch_to_artifact = {}
+        not_container_msg = 'Skipping archive {0} since it\'s not a container image'
+
+        for archive in archives:
+            if archive['btype'] != 'image':
+                log.debug(not_container_msg.format(archive['id']))
+                continue
+
+            architecture = archive['extra']['image']['arch']
+            if architecture in arch_to_artifact:
+                log.error(f'Build {build_id} has more than one container image with the arch '
+                          f'{architecture}')
+                continue
+
+            # Create or get the archive artifact that is the product of this build
+            artifact = self.create_or_update_archive_artifact_from_archive_info(archive)
+            arch_to_artifact[artifact.architecture] = artifact
+
+            # If an archive was created in the previous step, connect it to this build.
+            if not artifact.build.is_connected(parent_build):
+                artifact.build.connect(parent_build)
+
+        return arch_to_artifact
---
+++ tests/processor/test_container_analyzer.py
@@ -0,0 +1,169 @@
+# SPDX-License-Identifier: GPL-3.0+
+
+import mock
+import pytest
+
+from assayist.processor.container_analyzer import ContainerAnalyzer
+from tests.factories import BuildFactory, ArtifactFactory
+from tests.processor.test_main import IMAGE1, IMAGE2
+
+
+class TestContainerAnalyzerRun:
+    """Test container analysis against a common build."""
+
+    @pytest.fixture(scope='function', autouse=True)
+    def setup_build_with_artifacts(self):
+        """Create a container build with two archive artifacts with different architectures."""
+        build = BuildFactory.create(id_=774500, type_='container')
+
+        artifacts = []
+        for arch in ('x86_64', 's390x', 'ppc64le'):
+            artifacts.append(
+                ArtifactFactory.create(type_='container', architecture=arch)
+            )
+
+        for artifact in artifacts:
+            artifact.build.connect(build)
+
+    @mock.patch('assayist.processor.base.Analyzer.read_metadata_file')
+    @mock.patch('assayist.processor.container_analyzer.ContainerAnalyzer._create_or_update_parent')
+    def test_run_no_parent(self, mock_c_o_u_parent_build, mock_read_md_file):
+        """Test the ContainerAnalyzer.run function against a base image with no parent builds."""
+        # Minimal set of Brew build info metadata of a base image container (has no parents)
+        mock_read_md_file.return_value = {
+            'package_name': 'rhel-server-container',
+            'id': 774500,
+            'extra': {
+                'container_koji_task_id': 18568951,
+                'image': {
+                    'parent_image_builds': {},
+                },
+            },
+        }
+
+        analyzer = ContainerAnalyzer()
+        analyzer.run()
+
+        assert mock_c_o_u_parent_build.call_count == 0
+
+    @mock.patch('assayist.processor.base.Analyzer.read_metadata_file')
+    @mock.patch('assayist.processor.container_analyzer.ContainerAnalyzer._create_or_update_parent')
+    def test_run_one_parent(self, mock_c_o_u_parent_build, mock_read_md_file):
+        """Test the ContainerAnalyzer.run function against an image with one parent build."""
+        # Minimal set of Brew build info metadata of a container build that has one parent build
+        mock_read_md_file.return_value = {
+            'package_name': 'openshift-enterprise-base-container',
+            'id': 774500,  # Real ID: 787425
+            'extra': {
+                'container_koji_task_id': 18903380,
+                'image': {
+                    'parent_build_id': 742050,
+                    'parent_image_builds': {
+                        'rhel7:7-released': {
+                            'id': 742050,
+                            'nvr': 'rhel-server-container-7.5-424',
+                        },
+                    },
+                },
+            },
+        }
+
+        arch_to_artifacts = {arch: ArtifactFactory.create(type_='container', architecture=arch)
+                             for arch in ('x86_64', 's390x', 'ppc64le')}
+        mock_c_o_u_parent_build.return_value = arch_to_artifacts
+
+        analyzer = ContainerAnalyzer()
+        analyzer.run()
+
+        assert mock_c_o_u_parent_build.call_count == 1
+
+        # Check that all parent artifacts are linked to their respective artifacts by architecture
+        for arch, parent_artifact in arch_to_artifacts.items():
+            assert parent_artifact.artifacts_embedded_in.get().architecture == arch
+
+    @mock.patch('assayist.processor.base.Analyzer.read_metadata_file')
+    @mock.patch('assayist.processor.container_analyzer.ContainerAnalyzer._create_or_update_parent')
+    def test_run_multiple_parents(self, mock_c_o_u_parent_build, mock_read_md_file):
+        """Test the ContainerAnalyzer.run function against an image with multiple parent builds."""
+        # Minimal set of Brew build info metadata of a container build with multiple parent builds
+        mock_read_md_file.return_value = {
+            'package_name': 'openshift-enterprise-console-container',
+            'id': 774500,  # Real ID: 787432
+            'extra': {
+                'container_koji_task_id': 18903872,
+                'image': {
+                    'parent_build_id': 787425,
+                    'parent_image_builds': {
+                        'openshift/golang-builder:1.10': {
+                            'id': 780769,
+                            'nvr': 'openshift-golang-builder-container-1.10-1.10.3.6',
+                        },
+                        'openshift3/ose-base:v3.11.31.20181023.223156': {
+                            'id': 787425,
+                            'nvr': 'openshift-enterprise-base-container-v3.11.31-1',
+                        },
+                        'rhscl/nodejs-8-rhel7:1': {
+                            'id': 771613,
+                            'nvr': 'rh-nodejs8-container-1-30',
+                        },
+                    },
+                },
+            },
+        }
+
+        arch_to_embedded_artifacts = {
+            arch: ArtifactFactory.create(type_='container', architecture=arch)
+            for arch in ('x86_64', 's390x', 'ppc64le')
+        }
+
+        arch_to_buildroot_artifacts_1 = {
+            arch: ArtifactFactory.create(type_='container', architecture=arch)
+            for arch in ('x86_64', 's390x', 'ppc64le')
+        }
+
+        arch_to_buildroot_artifacts_2 = {
+            arch: ArtifactFactory.create(type_='container', architecture=arch)
+            for arch in ('x86_64', 's390x', 'ppc64le')
+        }
+
+        mock_c_o_u_parent_build.side_effect = [
+            arch_to_embedded_artifacts,
+            arch_to_buildroot_artifacts_1,
+            arch_to_buildroot_artifacts_2,
+        ]
+
+        analyzer = ContainerAnalyzer()
+        analyzer.run()
+
+        assert mock_c_o_u_parent_build.call_count == 3
+
+        # Check that all parent artifacts are embedded in their respective artifacts by architecture
+        for arch, parent_artifact in arch_to_embedded_artifacts.items():
+            assert parent_artifact.artifacts_embedded_in.get().architecture == arch
+
+        # Check that all parent artifacts are embedded as buildroot artifacts to their respective
+        # child artifacts.
+        for arch, parent_artifact in arch_to_buildroot_artifacts_1.items():
+            assert parent_artifact.artifacts_in_buildroot_for.get().architecture == arch
+
+        for arch, parent_artifact in arch_to_buildroot_artifacts_2.items():
+            assert parent_artifact.artifacts_in_buildroot_for.get().architecture == arch
+
+
+@mock.patch('assayist.processor.container_analyzer.get_koji_session')
+def test_create_or_update_parent_build_new(mock_get_koji_session):
+    """Test the ContainerAnalyzer._create_or_update_parent function for a new parent build."""
+    m_koji = mock.Mock()
+    m_koji.listArchives.return_value = [IMAGE1, IMAGE2]
+    mock_get_koji_session.return_value = m_koji
+
+    analyzer = ContainerAnalyzer()
+    arch_to_artifact_object = analyzer._create_or_update_parent(123456)
+
+    assert arch_to_artifact_object
+
+    image1_arch = IMAGE1['extra']['image']['arch']
+    assert arch_to_artifact_object[image1_arch].archive_id == IMAGE1['id']
+
+    image2_arch = IMAGE2['extra']['image']['arch']
+    assert arch_to_artifact_object[image2_arch].archive_id == IMAGE2['id']
