d8fcf7086713a114affa4becf30472c402e050e4
mprahl <mprahl@users.noreply.github.com>
2018-11-02 15:25:10 -0400

    Run the container RPM analyzer after the main analyzer

--- scripts/run-analyzers.py
+++ scripts/run-analyzers.py
@@ -4,12 +4,16 @@
 import argparse
 
 from assayist.processor.main_analyzer import MainAnalyzer
+from assayist.processor.container_rpm_analyzer import ContainerRPMAnalyzer
 
-parser = argparse.ArgumentParser(description='Run the main analyzer')
+parser = argparse.ArgumentParser(description='Run the Assayist analyzers on a Koji build')
 parser.add_argument('--input-dir', type=str,
                     help='The directory containing the "metadata" directory')
 args = parser.parse_args()
 
 input_dir = args.input_dir or '.'
 
+print('Running the main analyzer...')
 MainAnalyzer(input_dir).main()
+print('Running the container RPM analyzer...')
+ContainerRPMAnalyzer(input_dir).main()
