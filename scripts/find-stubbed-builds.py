13cdad2ab802bece0049b636e864ccdc7efaf005
mprpic <martin.prpic@gmail.com>
2018-11-14 14:46:45 +0100

    Add DB initialization for stub finder script

--- scripts/find-stubbed-builds.py
+++ scripts/find-stubbed-builds.py
@@ -2,12 +2,15 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 import argparse
+from neomodel import db
 
 from assayist.common.models.content import Build
+from assayist.processor.configuration import config
 
 parser = argparse.ArgumentParser(description='Return a list of stubbed builds')
 args = parser.parse_args()
 
+db.set_connection(config.DATABASE_URL)
 stubbed_builds = Build.nodes.has(source_location=False).all()
 
 for build in stubbed_builds:
